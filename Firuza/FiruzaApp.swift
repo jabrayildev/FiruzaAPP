1209129081320ecf76e1b853be5152771172b080
Jabrayil Gulamzada <c.gulamzadad@gmail.com>
2026-05-16 16:55:34 +0400

    Initial Commit

---
+++ Firuza/FiruzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiruzaApp.swift
+//  Firuza
+//
+//  Created by Джабраил on 16.05.26.
+//
+
+import SwiftUI
+
+@main
+struct FiruzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
